7351f4c903b05ce51a2b1305dd9d2a526c266389
Nivbenaviv <96342824+Nivbenaviv@users.noreply.github.com>
2022-07-28 15:04:18 +0300

    new node v

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
 import RecipesPage from './Components/Recipes/RecipesPage';
 import HomePage from './Components/Home/HomePage';
-// import axios from 'axios';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { useEffect } from 'react';
 function App() {
 
 
