33f4758be7efaf7665bb54355a55d0735a33b01e
Nivbenaviv <96342824+Nivbenaviv@users.noreply.github.com>
2022-07-28 13:50:52 +0300

    styled removed

--- src/Components/Navbar/NavbarComponent.js
+++ src/Components/Navbar/NavbarComponent.js
@@ -1,60 +1,24 @@
 import React from 'react'
-import styled from "styled-components"
+import styles from "./index.module.css"
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
  import { Link } from 'react-router-dom';
 const NavbarComponent = () => {
-  const MyNav=styled(Navbar)`
-  background-color:rgba(0,0,0,0.5);
-  height: 80px;
-  font-size: 20px;
-  color: white;  
-  ` 
-  const MyNavLink=styled(Link)`
-  font-size: 20px;
-  color: white;
-  text-transform: uppercase;
-  font-weight: bold;
-  transition: 0.2s ease-in;
-  text-decoration: none;
-  margin-left: 40px;
-  &:hover
-{
-  color: orange;
-}
-
-  ` 
-
-const MyNavBrand=styled(Navbar.Brand)`
-font-size: 30px;
-color: white;
- margin-right: 30px;
- transition: 0.2s ease-in;
- font-weight: bold;
-
- &:hover
-{
-  color: white;
-  transform: scale(1.1, 1.1);
-
-}
-` 
-
 
   return (
-    <MyNav expand="md">
+    <Navbar className={styles.nav} expand="md">
       <Container>
-        <MyNavBrand href="/">Home Chef</MyNavBrand>
+        <Navbar.Brand className={styles.navBrand} href="/">Home Chef</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <MyNavLink  as={Link} to="/">Home</MyNavLink>
-            <MyNavLink  as = {Link} to="/recipes">Recipes</MyNavLink>
+            <Nav.Link className={styles.navLink}  as={Link} to="/">Home</Nav.Link>
+            <Nav.Link className={styles.navLink} as = {Link} to="/recipes">Recipes</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
-    </MyNav>  )
+    </Navbar>  )
 }
 
 export default NavbarComponent
--- src/Components/Recipes/RecipeCard.js
+++ src/Components/Recipes/RecipeCard.js
@@ -1,52 +1,17 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
-import styled from 'styled-components'
 import styles from "./index.module.css"
 const RecipeCard = (props) => {  
-   const Header=styled(Card.Title)`
-   text-align: center;
-   font-size: 26px;
-   font-weight: bold;
-   margin-top: 20px;
-   `
-   
-    const MyCard=styled(Card)`
-    width:23rem;
-    margin:0;
-    margin-top:60px;
-    height:25rem;
-    transition: all 0.3s ease-in;
-    columns:black;
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
-    &:hover
-    {
-       cursor: pointer;
-       transform: scale(1.1, 1.1);
-      background-color: orange;
-       color: white;
-    }
-    `
-    const ReacipesImg =styled.img`
-         src:'${props.img}';
-         width:100%;
-         max-width: fit-content;
-         max-height: 16rem;
-         margin-left:-15px;
-         height:100%;
-         margin-bottom: -15px;
-         margin-right: -15px;
-         
-    `
     
       return (
-       <MyCard>
+       <Card className={styles.card}>
        <a className={styles.a} href={props.src}>
-       <Header>{props.title}</Header>   
+       <Card.Title className={styles.header}>{props.title}</Card.Title>   
        </a>
-       <Card.Body>
-       <ReacipesImg src={props.img} alt={props.alt} />
+       <Card.Body className={styles.cardBody}>
+       <img className={styles.reacipesImg} src={props.img} alt={props.alt} />
        </Card.Body>
-     </MyCard>  
+     </Card>  
    
      )
 }
